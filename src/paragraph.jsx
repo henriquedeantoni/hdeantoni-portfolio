a5e8f5c8b29146d78e65105c918e21ae95a874ac
henrideanton <henriqantoni@gmail.com>
2024-01-20 21:34:09 -0300

    primeiro commit

---
+++ src/paragraph.jsx
@@ -0,0 +1,12 @@
+function divParagraph(){
+
+    return(
+        <>
+            <div>
+                <p>este é o inicio do meu portfolio em Vite + React</p>
+            </div>
+        </>
+    )
+}
+
+export default divParagraph;

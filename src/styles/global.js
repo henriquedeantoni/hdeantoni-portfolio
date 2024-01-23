389ba787cfd17db4f5f7902c122932d0ebcc6e73
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-01-22 23:27:01 -0300

    Create global.js

---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyles`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  body, input, button{
+    font: 1rem 'League+Spartan', sans-serif;
+  }
+  h1, h2, p, span, strong, button, label, input {
+    line-height: 100%;
+  }
+  button {
+    cursor: pointer;
+  }
+`
